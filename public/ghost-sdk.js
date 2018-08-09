1cc32090591b390246e6d1f3da644cb4552006af
Jonathan Martin <me@nybblr.com>
2018-08-09 08:32:43 -0400

    Update all blog links to point to www.

--- public/ghost-sdk.js
+++ public/ghost-sdk.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    var apiUrl = '//blog.yellowscale.com/ghost/api/v0.1/',
+    var apiUrl = '//www.yellowscale.com/ghost/api/v0.1/',
         clientId,
         clientSecret,
         url,
